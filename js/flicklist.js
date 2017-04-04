2cbe8bf1f6190967b5b5d4daa81eab1c5a870dc0
Jun Ma <majun612@hotmail.com>
2017-04-03 22:25:29 -0500

    studio4 done

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,13 +8,13 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "cfe40e8b2740ff789f2e45958bea74bc", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
   posterUrl: function(movie) {
     var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
-    return baseImageUrl + movie.poster_path; 
+    return baseImageUrl + movie.poster_path;
   }
 }
 
@@ -28,9 +28,10 @@ var api = {
 
 // TODO 1
 // this function should accept a second argument, `keywords`
-function discoverMovies(callback) {
 
-  // TODO 2 
+function discoverMovies(callback,keywords) {
+
+  // TODO 2
   // ask the API for movies related to the keywords that were passed in above
   // HINT: add another key/value pair to the `data` argument below
 
@@ -38,6 +39,7 @@ function discoverMovies(callback) {
     url: api.root + "/discover/movie",
     data: {
       api_key: api.token,
+      with_keywords: keywords,
     },
     success: function(response) {
       model.browseItems = response.results;
@@ -48,7 +50,7 @@ function discoverMovies(callback) {
 
 
 /**
- * Makes an AJAX request to the /search/keywords endpoint of the API, using the 
+ * Makes an AJAX request to the /search/keywords endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, invokes the supplied callback function, passing in
@@ -70,7 +72,7 @@ function searchMovies(query, callback) {
 
 
   // TODO 4b
-  // create a new variable called keywordsString by converting 
+  // create a new variable called keywordsString by converting
   // the array of ids to a comma-separated string, e.g.
   //      "192305,210090,210092,210093"
   // HINT: use the Array join function
@@ -84,23 +86,31 @@ function searchMovies(query, callback) {
 
 
   // TODO 4d
-  // when the response comes back, call discoverMovies, 
+  // when the response comes back, call discoverMovies,
   // passing along 2 arguments:
-  // 1) the callback 
+  // 1) the callback
   // 2) the string of keywords
 
 
   $.ajax({
-    url: api.root + "/search/movie",
+    url: api.root + "/search/keyword",
     data: {
       api_key: api.token,
       query: query
     },
     success: function(response) {
       console.log(response);
+      var keywordsString = response.results.map(function(ids){
+                          return ids.id;
+                      }).join('|');
+     discoverMovies(callback,keywordsString)
     }
+    // keywordsString = keywordIDs.join('|');
   });
+  // discoverMovies(callback,keywordsString)
 }
+
+
 
 
 /**
@@ -115,7 +125,7 @@ function render() {
   // render watchlist items
   model.watchlistItems.forEach(function(movie) {
     var title = $("<h6></h6>").text(movie.original_title);
-      
+
     // movie poster
     var poster = $("<img></img>")
       .attr("src", api.posterUrl(movie))
@@ -135,7 +145,7 @@ function render() {
     var panelHeading = $("<div></div>")
       .attr("class", "panel-heading")
       .append(title);
-    
+
     // panel body contains the poster and button
     var panelBody = $("<div></div>")
       .attr("class", "panel-body")
@@ -168,7 +178,7 @@ function render() {
     var itemView = $("<li></li>")
       .attr("class", "list-group-item")
       .append( [title, overview, button] );
-      
+
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
